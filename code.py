2c51f2db2bf40e8efc9936e60d5011daf55c1c33
BonShaun <54891459+BonShaun@users.noreply.github.com>
2019-11-12 17:56:44 -0500

    update

--- code.py
+++ code.py
@@ -1,13 +1,32 @@
 import urllib.request
+import urllib.parse
 import json
 from pprint import pprint
 
 MAPQUEST_API_KEY = 'Ing5xxWtWc0FrjDV2W0vA7EVMFtiIj7N'
+MBTA_API_KEY : '6018a853c6e8432694c83919c91e0892'
 
-url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
-f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
-response_data = json.loads(response_text)
-pprint(response_data['results'][0]['locations'][0]['displayLatLng'])
+def ask_location(loc):
+    d = {}
+    d['location'] = loc
+    variable = urllib.parse.urlencode(d)
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&{variable}'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    latlng = (response_data['results'][0]['locations'][0]['displayLatLng'])
+    return latlng
 
+latlng = ask_location('Babson College')
+print(latlng)
+lat = latlng['lat']
+lng = latlng['lng']
+index = 0
+
+
+
+# url_MBTA = f'https://api-v3.mbta.com/docs/swagger/swagger.json/data/{index}/attributes/latitude={lat}/data/{index}/attributes/longitude={lng}'
+# f_MBTA = urllib.request.urlopen(url_MBTA)
+# response_text_MBTA = f_MBTA.read().decode('utf-8')
+# response_data_MBTA = json.loads(response_text_MBTA)
 
